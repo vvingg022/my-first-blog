7715cdc199f257420ea004b36588f7acafa35926
Vaibhavi <vaibhavigawai22@gmail.com>
2026-09-09 11:42:31 +0000

    correction

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,20 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -23,5 +37,5 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
