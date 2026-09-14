27cc685b270c216f8234a463f6b141aebd53cf3c
Vaibhavi <vaibhavigawai22@gmail.com>
2026-09-14 10:45:30 +0000

    fixed

--- blog/views.py
+++ blog/views.py
@@ -48,3 +48,9 @@ def post_publish(request, pk):
     if request.method=='POST':
         post.publish()
     return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.delete()
+    return redirect('post_list')
